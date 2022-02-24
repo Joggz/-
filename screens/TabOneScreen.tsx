1fc4c68bc863db9efede2bcb81733c7bad836680
laoye_jnr <idrisaboladeo@gmail.com>
2022-02-24 13:24:31 +0100

    :construction_worker: чат тело

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,32 +1,21 @@
-import { StyleSheet } from 'react-native';
+import { Text, View } from "react-native";
+import { RootTabScreenProps } from "../types";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
+import ScrollableTabView, {
+  ScrollableTabBar,
+} from "react-native-scrollable-tab-view";
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+export default function TabOneScreen({
+  navigation,
+}: RootTabScreenProps<"TabOne">) {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
+    <ScrollableTabView
+      style={{ marginTop: 10 }}
+      initialPage={0}
+      renderTabBar={() => <ScrollableTabBar />}
+    >
+      <Text tabLabel="Chat">Chat</Text>
+      <Text tabLabel="Calls">Calls</Text>
+    </ScrollableTabView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
